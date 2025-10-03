6b4d61e4e13bf6f26684960c769c671a4b0b2f86
Jonathan Garcia <149093672+jonathanreinaldo@users.noreply.github.com>
2025-10-02 22:18:26 -0500

    Update acordeon.js

--- js/acordeon.js
+++ js/acordeon.js
@@ -1,74 +1,80 @@
                         
-const respuesta0=document.getElementById("respuesta0");
-const respuesta1=document.getElementById("respuesta1");
-const respuesta2=document.getElementById("respuesta2");
-const respuesta3=document.getElementById("respuesta3");
+const respuesta0 = document.getElementById("respuesta0")
+const respuesta1 = document.getElementById("respuesta1")
+const respuesta2= document.getElementById("respuesta2")
+const respuesta3= document.getElementById("respuesta3")
+
+const logoNegativo0 = document.getElementById("logoMinus0")
+const logoPositivo0 = document.getElementById("logoPlus0")
+
+const logoNegativo1 = document.getElementById("logoMinus1")
+const logoPositivo1 = document.getElementById("logoPlus1")
+
+ const logoNegativo2 = document.getElementById("logoMinus2")
+ const logoPositivo2 = document.getElementById("logoPlus2")
+
+ const logoNegativo3 = document.getElementById("logoMinus3")
+ const logoPositivo3 = document.getElementById("logoPlus3")
+
+const respuestas = [respuesta0, respuesta1, respuesta2, respuesta3 ]
  
-const logoPlus=document.getElementById("logoPlus")
-const logoMinus=document.getElementById("logoMinus")
+   function botn0(){
 
-const logos = [logoPlus, logoMinus]
-const respuestas = [respuesta0, respuesta1, respuesta2, respuesta3]
+       if( respuestas[0].style.display == "block"){
+           respuestas[0].style.display = "none" 
 
-function boton0() {
-      
-     if( respuestas[0].style.display == "none"){
-        
-          respuestas[0].style.display = "block"
-         logoPlus.style.display="none"
-          logoMinus.style.display="block"
-
-     }else{
-         respuestas[0].style.display = "none"
-            logoPlus.style.display="block"
-          logoMinus.style.display="none"
+            logoPositivo0.style.display = "block"
+           logoNegativo0.style.display = "none"
           
-     } 
-}
+      }else{
+            respuestas[0].style.display = "block"
+            logoPositivo0.style.display = "none"
+             logoNegativo0.style.display = "block"
+      }
+  };
+
+  function botn1(){
+     if( respuestas[1].style.display == "block"){
+           respuestas[1].style.display = "none" 
+
+            logoPositivo1.style.display = "block"
+           logoNegativo1.style.display = "none"
+          
+      }else{
+            respuestas[1].style.display = "block"
+            logoPositivo1.style.display = "none"
+             logoNegativo1.style.display = "block"
+      }
+  }
+
+   function botn2(){
+       if( respuestas[2].style.display == "block"){
+           respuestas[2].style.display = "none" 
+
+            logoPositivo2.style.display = "block"
+           logoNegativo2.style.display = "none"
+          
+      }else{
+            respuestas[2].style.display = "block"
+            logoPositivo2.style.display = "none"
+             logoNegativo2.style.display = "block"
+      }
+
+   }
+
+    function botn3(){
+      if( respuestas[3].style.display == "block"){
+           respuestas[3].style.display = "none" 
+
+            logoPositivo3.style.display = "block"
+           logoNegativo3.style.display = "none"
+          
+      }else{
+            respuestas[3].style.display = "block"
+            logoPositivo3.style.display = "none"
+             logoNegativo3.style.display = "block"
+      }
+
+   }
  
- function boton1(){
-     if( respuestas[1].style.display =="none" ){
-        
-         respuestas[1].style.display="block"
-            logoPlus.style.display="none"
-          logoMinus.style.display="block"
-
-     }else{
-         respuestas[1].style.display = "none"
-            logoPlus.style.display="block"
-          logoMinus.style.display="none"
-     }
-
- }
-
- function boton2(){
-     if( respuestas[2].style.display =="none" ){
-        
-
-         respuestas[2].style.display="block"
-          logos.style.display="none"
-          logos.style.display="block"
-
-     }else{
-
-         respuestas[2].style.display = "none"
-          logos.style.display="none"
-          logos.style.display="block"
-     }
-
- }
-
- function boton3(){
-     if( respuestas[3].style.display =="none" ){
-        
-         respuestas[3].style.display="block"
-          logos.style.display="none"
-          logos.style.display="block"
-
-     }else{
-         respuestas[3].style.display = "none"
-          logos.style.display="none"
-          logos.style.display="block"
-     }
-
- }
+ 
